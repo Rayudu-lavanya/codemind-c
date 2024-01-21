c20eee391c331646a9b9792711bc9bd639998803
Rayudu-lavanya <155358858+Rayudu-lavanya@users.noreply.github.com>
2024-01-21 22:29:10 +0530

    #include <stdio.h> int main() {     int a,b,c;     scanf("%d%d",&a,&b); int Sum = a+b; printf("Sum:%d ",a+b); int Difference = a-b; printf("Difference:%d ",a-b); int Product = a*b; printf("Product:%d ",a*b); int Quotient = a/b; printf("Quotient:%d ",a/b); int Remainder = a%b; printf("Remainder:%d ",a%b); return 0; }

---
+++ Arithmetic_operations.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c;
+    scanf("%d%d",&a,&b);
+int Sum = a+b;
+printf("Sum:%d
+",a+b);
+int Difference = a-b;
+printf("Difference:%d
+",a-b);
+int Product = a*b;
+printf("Product:%d
+",a*b);
+int Quotient = a/b;
+printf("Quotient:%d
+",a/b);
+int Remainder = a%b;
+printf("Remainder:%d
+",a%b);
+return 0;
+}
